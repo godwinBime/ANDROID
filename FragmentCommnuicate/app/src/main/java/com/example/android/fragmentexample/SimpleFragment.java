503c0ae57e0f2683cf47de545f7d2869ee30804c
godwinBime <tardzenyuy1989@gmail.com>
2022-05-20 08:32:27 -0400

    'FragmentCommnuicate'

---
+++ FragmentCommnuicate/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -0,0 +1,154 @@
+package com.example.android.fragmentexample;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SimpleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SimpleFragment extends Fragment {
+    OnFragmentInteractionListener mListener;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private static final String CHOICE = "choice";
+
+    //Button states
+    private static final int YES = 0;
+    private static final int NO = 1;
+    private static final int NONE = 2;
+
+    //Radio button variable
+    public int mRadioButtonChoice = NONE;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    interface OnFragmentInteractionListener{
+        void onRadioButtonChoice(int choice);
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SimpleFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SimpleFragment newInstance(String param1, String param2) {
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static SimpleFragment newInstance(int choice){
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(CHOICE, choice);
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_simple, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+        final RatingBar ratingBar = rootView.findViewById(R.id.rating_bar_id);
+
+        if (getArguments().containsKey(CHOICE)){
+            //A choice was made, so get the choice
+            mRadioButtonChoice = getArguments().getInt(CHOICE);
+
+            //Check the radio button choice.
+            if (mRadioButtonChoice != NONE){
+                radioGroup.check(radioGroup.getChildAt(mRadioButtonChoice).getId());
+            }
+        }
+
+        //Set the radioGroup onCheckChanged listener.
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                View radioButton = radioGroup.findViewById(checkedId);
+                int index = radioGroup.indexOfChild(radioButton);
+                TextView textView = rootView.findViewById(R.id.fragment_header);
+                switch (index){
+                    case YES://user chose "YES."
+                        textView.setText(R.string.yes_message);
+                        mRadioButtonChoice = YES;
+                        mListener.onRadioButtonChoice(YES);
+                        break;
+                    case NO://User chose "NO"
+                        textView.setText(R.string.no_message);
+                        mRadioButtonChoice = NO;
+                        mListener.onRadioButtonChoice(NO);
+                        break;
+                    default://no choice
+                        mRadioButtonChoice = NONE;
+                        mListener.onRadioButtonChoice(NONE);
+                        break;
+                }
+            }
+        });
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                /*
+                  Get rating and show toast with rating
+                  */
+                String myRating = (getString(R.string.my_rating) + ratingBar.getRating());
+                Toast.makeText(getContext(), myRating, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /*
+         * Return the view of the fragment UI
+         * */
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener){
+            mListener = (OnFragmentInteractionListener) context;
+        }else {
+            throw new ClassCastException(context.toString() + getResources().getString(R.string.exception_message));
+        }
+    }
+}
