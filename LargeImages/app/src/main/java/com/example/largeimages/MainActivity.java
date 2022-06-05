28c4732aa3793d1e86d63ced11235fa5fb4621bb
godwinBime <tardzenyuy1989@gmail.com>
2022-06-05 02:57:32 -0400

    LargeImages

---
+++ LargeImages/app/src/main/java/com/example/largeimages/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.largeimages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int toggle = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeImage(View view){
+        if (toggle == 0){
+            view.setBackgroundResource(R.drawable.dinosaur_medium);
+            toggle = 1;
+        }else {
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            view.setBackgroundResource(R.drawable.ankylo);
+            toggle = 0;
+        }
+    }
+}
